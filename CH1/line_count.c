ef4181439de4cb125c5775ed22ae1436b4370ffd
Rafael Goncalves <rafael.nog.251@gmail.com>
2016-01-04 15:05:11 -0200

    added count and line count

---
+++ CH1/line_count.c
@@ -0,0 +1,14 @@
+#include <stdi.o>
+//count lines in input
+
+void main() {
+    int c, nl;
+    nl = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c=='\n')
+            ++nl;
+    }
+    printf("%d\n");
+}
+
